ee4057cece81c8cc7b89fe3e72bc8e591602fa96
Nate Elliott <nelliott@pcopen.net>
2019-10-22 13:09:55 -0700

    initial thunk setup

--- src/Utils/TestUtils.js
+++ src/Utils/TestUtils.js
@@ -1,9 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import rootReducer from '../reducers';
+import { middlewares } from './../configureStore';
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddle = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddle(rootReducer, initialState);
 }
 
 export const findByTestAttr = (wrapper, val) => {
