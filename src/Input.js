068b2266cb609b80022dce9e64df7421704f6251
Nate Elliott <nelliott@pcopen.net>
2019-10-22 11:50:50 -0700

    completed input with test

--- src/Input.js
+++ src/Input.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
-    return <div />
+    const contents = this.props.success ? null : (
+      <form className='form-inline'>
+        <input data-test='input-box' className='mb-2 mx-sm-3' type='text' placeHolder='Enter Guess' />
+        <button data-test="submit-button" className="btn btn-primary mb-2" type="submit" children="Submit"/>
+      </form>
+    );
+    return (
+      <div data-test="component-input">
+        {contents}
+      </div>
+    );
   }
-};
-
-const mapStateToProps = (state) => {
-  return {};
 }
 
-export default connect(mapStateToProps)(Input)
+const mapStateToProps = ({success}) => {
+  return {success};
+};
+
+export default connect(mapStateToProps)(Input);
